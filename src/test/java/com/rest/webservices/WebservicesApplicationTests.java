5349ac70cf302b97bb5f0f44ad51fcab76225502
Hamza BAROUDI <61873638+baroudu1@users.noreply.github.com>
2022-08-14 13:39:05 +0100

    initialisation the webSrvice

---
+++ src/test/java/com/rest/webservices/WebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rest.webservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebservicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
